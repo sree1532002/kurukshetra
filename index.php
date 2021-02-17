0ccc9185afe92fcbfa54da5d00314b601bfe8754
ratchabala <ratchabala@gmail.com>
2021-02-18 00:42:00 +0530

    basic

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Muruga Bhavan</title>
+  <link rel="icon" href="bhavan.jpeg" type="image/icon type">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="stylefooter.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body id ="bg">
+    <div class="bg-img" class="float-container">
+        <div id = "t" class="float-child">
+            <img src = "bhavan.jpeg" id="image">
+        </div>
+        <div id = "p" class="float-child">
+            <ul class="nav justify-content-end">
+                <li class="nav-item" >
+                <a class="nav-link"  id = "item" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" id = "item" href="#">Bookings</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" id = "item" href="#menu">Menu</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" id = "item" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <h1 id = "topic">Hotel Muruga Bhavan</h1>
+    </div>
+    <div id="about">
+        <h1 id="heading">Legacy of MB</h1>
+        <p>
+        Three decades ago, a small sweet shop was started by Late K.S. Thirupathi Raja, in the metropolis of Chennai. This same shop has now flowered into multiple branches that dot the length and breadth of Chennai as well as its neighbouring cities.
+
+M/s. Muruga Bhavan Sweets & Snacks is now a well-established brand in Southern India that continues to grow with a vision to capture the global market. The baton of this sweet legacy was passed on by K.S. Thirupathi Raja to his sons, Mr. K.T. Venkatesan and Mr. K.T. Srinivasa Raja who now act as the driving force behind MB.
+
+The MB family is synonymous with a commitment to quality and a high degree of professionalism by using state of the art equipment in the manufacture of sweets, savouries, snacks and myriad varieties of Indian food. It is the taste of our food, coupled with our commitment to hygiene that makes us who we are today- a brand renowned for our authenticity. Today, MB is the most recognised Vegetarian restaurant in Chennai, cherished by many for our bountiful food at affordable prices!
+    </p>
+    <h1 id="heading">Quality you can taste</h1>
+        <p>We serve delicious vegetarian food made using fresh, quality ingredients catering to a multitude of taste preferences, without burning a hole in your wallet! We’re big on flavour and uncompromising on service, so much so that our restaurants are reckoned to be amongst the best vegetarian restaurants in the country.
+
+Our sweets and snacks range from the melt-in-your-mouth Mysorepak from the south, the multi-variety Malaipeda of the North, the mouth-watering Badam Halwa of the East or the timeless dry fruits of the West. Our products are synonymous with traditional taste that you know and love while pioneering in the use of the latest techniques in manufacturing.
+
+Our restaurants serve South Indian, North Indian as well Chinese delicacies! We use the finest ingredients that allows us to serve the freshest quality food. We give utmost importance to hygiene and customer service, which will leave you wanting more!</p>
+    </div>
+<div id = "cards">
+    <div class="gallery" alt = "food">
+        <img src="food.jpg">
+    <div class="desc">Food</div>
+    </div>
+    <div class="gallery" alt = "food">
+        <img src="food.jpg">
+    <div class="desc">Food</div>
+    </div>
+    <div class="gallery" alt = "food">
+        <img src="food.jpg">
+    <div class="desc">Food</div>
+    </div>
+    <div class="gallery" alt = "food">
+        <img src="food.jpg">
+    <div class="desc">Food</div>
+    </div>
+    <div class="gallery" alt = "food">
+        <img src="food.jpg">
+    <div class="desc">Food</div>
+    </div>
+    <div class="gallery" alt = "food">
+        <img src="food.jpg">
+    <div class="desc">Food</div>
+    </div>
+</div>
+</body>
+</html>
