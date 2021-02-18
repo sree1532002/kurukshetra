911782b8d48753557d959430b9308a7b32c653c9
ratchabala <ratchabala@gmail.com>
2021-02-18 09:46:57 +0530

    style

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
   <link rel="icon" href="bhavan.jpeg" type="image/icon type">
   <meta charset="utf-8">
   <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="stylefooter.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
