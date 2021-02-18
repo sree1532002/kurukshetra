24f50a74ea98d5e5775b5a26cf9e71383aaf247e
ratchabala <ratchabala@gmail.com>
2021-02-18 18:04:46 +0530

    try

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <link rel="icon" href="bhavan.jpeg" type="image/icon type">
   <meta charset="utf-8">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="stylecards.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -13,7 +14,8 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body id ="bg">
+
+<body>
     <div class="bg-img" class="float-container">
         <div id = "t" class="float-child">
             <img src = "bhavan.jpeg" id="image">
@@ -21,13 +23,13 @@
         <div id = "p" class="float-child">
             <ul class="nav justify-content-end">
                 <li class="nav-item" >
-                <a class="nav-link"  id = "item" href="#">Home</a>
+                <a class="nav-link"  id = "item" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" id = "item" href="#bg">Menu</a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" id = "item" href="#">Bookings</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" id = "item" href="#menu">Menu</a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" id = "item" href="#">Contact</a>
@@ -36,47 +38,144 @@
         </div>
         <h1 id = "topic">Hotel Muruga Bhavan</h1>
     </div>
-    <div id="about">
+    <div id="about" class = "grid-container">
+    <div class = "grid-child">
         <h1 id="heading">Legacy of MB</h1>
-        <p>
+        
         Three decades ago, a small sweet shop was started by Late K.S. Thirupathi Raja, in the metropolis of Chennai. This same shop has now flowered into multiple branches that dot the length and breadth of Chennai as well as its neighbouring cities.
 
 M/s. Muruga Bhavan Sweets & Snacks is now a well-established brand in Southern India that continues to grow with a vision to capture the global market. The baton of this sweet legacy was passed on by K.S. Thirupathi Raja to his sons, Mr. K.T. Venkatesan and Mr. K.T. Srinivasa Raja who now act as the driving force behind MB.
 
 The MB family is synonymous with a commitment to quality and a high degree of professionalism by using state of the art equipment in the manufacture of sweets, savouries, snacks and myriad varieties of Indian food. It is the taste of our food, coupled with our commitment to hygiene that makes us who we are today- a brand renowned for our authenticity. Today, MB is the most recognised Vegetarian restaurant in Chennai, cherished by many for our bountiful food at affordable prices!
-    </p>
+
+</div>
+<div class = "grid-child">
     <h1 id="heading">Quality you can taste</h1>
-        <p>We serve delicious vegetarian food made using fresh, quality ingredients catering to a multitude of taste preferences, without burning a hole in your wallet! We’re big on flavour and uncompromising on service, so much so that our restaurants are reckoned to be amongst the best vegetarian restaurants in the country.
+    We serve delicious vegetarian food made using fresh, quality ingredients catering to a multitude of taste preferences, without burning a hole in your wallet! We’re big on flavour and uncompromising on service, so much so that our restaurants are reckoned to be amongst the best vegetarian restaurants in the country.
 
 Our sweets and snacks range from the melt-in-your-mouth Mysorepak from the south, the multi-variety Malaipeda of the North, the mouth-watering Badam Halwa of the East or the timeless dry fruits of the West. Our products are synonymous with traditional taste that you know and love while pioneering in the use of the latest techniques in manufacturing.
 
-Our restaurants serve South Indian, North Indian as well Chinese delicacies! We use the finest ingredients that allows us to serve the freshest quality food. We give utmost importance to hygiene and customer service, which will leave you wanting more!</p>
-    </div>
-<div id = "cards">
-    <div class="gallery" alt = "food">
-        <img src="food.jpg">
-    <div class="desc">Food</div>
-    </div>
-    <div class="gallery" alt = "food">
-        <img src="food.jpg">
-    <div class="desc">Food</div>
-    </div>
-    <div class="gallery" alt = "food">
-        <img src="food.jpg">
-    <div class="desc">Food</div>
-    </div>
-    <div class="gallery" alt = "food">
-        <img src="food.jpg">
-    <div class="desc">Food</div>
-    </div>
-    <div class="gallery" alt = "food">
-        <img src="food.jpg">
-    <div class="desc">Food</div>
-    </div>
-    <div class="gallery" alt = "food">
-        <img src="food.jpg">
-    <div class="desc">Food</div>
+Our restaurants serve South Indian, North Indian as well Chinese delicacies! We use the finest ingredients that allows us to serve the freshest quality food. We give utmost importance to hygiene and customer service, which will leave you wanting more!
     </div>
 </div>
+    <div id="bg">
+        <br><br>
+        <h1 class="mm">Menu</h1>
+    <div class="slideshow-container" id = "card">
+
+<div class="mySlides fade">
+  <img src="https://www.holidify.com/images/cmsuploads/compressed/indian-1768906_1920_20180322173733.jpg" id="cardimg"style="width:100%;height:auto">
+
+</div>
+
+<div class="mySlides fade">
+  <img src="https://6d1bdf0e0edb084eae10-5bfabe5484726969ac662c6d377e2f3c.ssl.cf2.rackcdn.com/themes/img/southindian-recipe1.jpg" id="cardimg"style="width:100%;height:auto">
+
+</div>
+
+<div class="mySlides fade">
+
+  <img src="https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/content6553.jpg" id="cardimg"style="width:100%;height:auto">
+  
+</div>
+
+<div class="mySlides fade">
+ 
+  <img src="https://lh3.googleusercontent.com/proxy/cCAeLN_UMTKBX-MbAx8kkDrzMCwgxedkJ--g52A6GlDh_yS5c3YV0tULvmFgsYSMGrcbUuQfONDd-qRgCvcEcVA4HpR1wf5Lt31nyfqUvXbMI36ip7tTXEeKAluDNde_xiKL9Tjm" id="cardimg"style="width:100%;height:auto">
+  
+</div>
+
+<div class="mySlides fade">
+
+  <img src="http://2.bp.blogspot.com/-1Vy8ilCzfok/TacfNwOU_TI/AAAAAAAAAM8/50Y8NDG2FM8/s1600/Samabr.jpg" id="cardimg"style="width:100%;height:auto">
+  
+</div>
+
+<div class="mySlides fade">
+ 
+  <img src="https://media.istockphoto.com/photos/indian-butter-chicken-curry-picture-id177043240?k=6&m=177043240&s=612x612&w=0&h=h9_Y07ppShzZCbbuJXHX2y_ob1v6rQNP7r8467VOhb0=" id="cardimg"style="width:100%;height:auto">
+  
+</div>
+<div class="mySlides fade">
+  
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr-eR9j8qiOxVGik6R5xaQrDBY8mZbcWZIfg&usqp=CAU" id="cardimg"style="width:100%;height:auto">
+  
+</div>
+<div class="mySlides fade">
+
+  <img src="https://cdn.shopify.com/s/files/1/1726/9835/articles/Indian_Sweets_1024x1024.jpeg?v=1517221761" id="cardimg"style="width:100%;height:auto">
+
+</div>
+<div class="mySlides fade">
+
+  <img src="https://images.outlookindia.com/public/uploads/articles/2017/11/14/rosogolla_571_855.jpg" id="cardimg"style="width:100%;height:auto">
+  
+</div>
+<div class="mySlides fade">
+
+  <img src="https://i.ndtvimg.com/i/2016-03/kaju-barfi-625_625x350_81459346355.jpg" id="cardimg"style="width:100%;height:auto">
+  
+</div>
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span>
+  <span class="dot"></span>
+  <span class="dot"></span>
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+<div>
+    <br>
+<img src = "menucard.jpeg" id="menucard1">
+<img src = "menucard.jpeg" id="menucard2">
+</div>
+</div>
+<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
 </body>
 </html>
