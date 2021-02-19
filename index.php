48bd4ed6a19a2e3fa708dd449b6401030d166357
ratchabala <ratchabala@gmail.com>
2021-02-19 16:37:31 +0530

    footer

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="stylecards.css">
+  <link rel="stylesheet" href="footer.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -13,6 +14,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -32,7 +34,7 @@
                 <a class="nav-link" id = "item" href="#">Bookings</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" id = "item" href="#">Contact</a>
+                <a class="nav-link" id = "item" href="#contact">Contact</a>
                 </li>
             </ul>
         </div>
@@ -131,11 +133,10 @@ Our restaurants serve South Indian, North Indian as well Chinese delicacies! We 
   <span class="dot"></span> 
   <span class="dot"></span> 
 </div>
-<div>
-    <br>
-<img src = "menucard.jpeg" id="menucard1">
-<img src = "menucard.jpeg" id="menucard2">
-</div>
+<br><br>
+<button type="button" class="btn btn-danger" id="moremenu">More...</button>
+<br>
+<br>
 </div>
 <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 <script>
@@ -177,5 +178,13 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 </script>
+<div class = "footer" id="contact">
+  <i class="fa fa-phone" id="icon1"></i><a id="footericons" href="#">179823728932</a>
+  <i class="fa fa-map-marker" id="icon2"></i><a id="footericons" href="#">8237829732</a>
+  <i class="fa fa-envelope" id="icon3"></i><a id="footericons" href="mailto:ab@gmail.com">abc@gmail.com</a><br>
+  <a href="https://www.google.com/search?tbs=lf:1,lf_ui:1&tbm=lcl&q=location+anna+university&rflfq=1&num=10&ved=2ahUKEwj9ke3a4vXuAhUUOSsKHUGAC0gQtgN6BAgTEAc#rlfi=hd:;si:;mv:[[13.0262271,80.2418169],[13.005884799999999,80.20306529999999]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3,lf:1,lf_ui:1">
+  <img src="map.jpeg" alt="map" id="mapres"></a>
+<p id="copy">@Copyright 2021, Hotel Muruga Bhavan</p><br><br>
+</div>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,5 @@
+div>
+    <br>
+<img src = "menucard.jpeg" id="menucard1">
+<img src = "menucard.jpeg" id="menucard2">
+</div>
